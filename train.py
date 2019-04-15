82e998697bd6b100501e46a4fed11f5510988ff2
mRcfps <1402491442@qq.com>
2019-04-15 11:46:14 +0800

    Enable training by epochs

--- train.py
+++ train.py
@@ -1,6 +1,11 @@
 import argparse
+import os
+import warnings
+
+warnings.filterwarnings('ignore')
 
 import numpy as np
+from tqdm import tqdm
 
 import torch
 import torch.nn as nn
@@ -9,29 +14,49 @@ from torchvision.transforms import ToTensor
 from torchvision.models import vgg13
 
 import config
-from data import SuperpixelDataset
+from utils.data import SuperpixelDataset
 from wessup import Wessup
 
 
+def build_cli_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dataset_path', help='Path to dataset')
+    parser.add_argument('--gpu', action='store_true', default=False,
+                        help='Whether to use gpu')
+    parser.add_argument('-e', '--epochs', type=int, default=100,
+                        help='Number of training epochs')
+    parser.add_argument('-m', '--message', help='Note on this experiment')
+
+    return parser
+
+
 if __name__ == '__main__':
-    dataset = SuperpixelDataset('data/train')
+    parser = build_cli_parser()
+    args = parser.parse_args()
+
+    device = 'cuda' if args.gpu else 'cpu'
+    dataset = SuperpixelDataset(os.path.join(args.dataset_path, 'train'))
     dataloader = torch.utils.data.DataLoader(
-        dataset, batch_size=1, shuffle=True)
+        dataset, batch_size=1, shuffle=True, num_workers=os.cpu_count() // 2)
 
     vgg = vgg13(pretrained=True)
-    wessup = Wessup(vgg.features, 'cpu')
+    wessup = Wessup(vgg.features, device=device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(wessup.parameters(), lr=0.001, momentum=0.9)
 
-    for idx, data in enumerate(dataloader):
-        img, sp_maps, sp_labels = data
-        optimizer.zero_grad()
-        sp_pred = wessup(img, sp_maps.squeeze())
-        loss = criterion(sp_pred, sp_labels.squeeze())
-        loss.backward()
-        optimizer.step()
+    for epoch in range(args.epochs):
+        print('Epoch {}/{}'.format(epoch + 1, args.epochs))
+        print('-' * 10)
 
-        print(f'[{idx}] loss: {loss.item()}')
+        for img, sp_maps, sp_labels in tqdm(dataloader):
+            img = img.to(device)
+            sp_maps = sp_maps.to(device)
+            sp_labels = sp_labels.to(device)
 
-        if idx > 4:
-            break
+            optimizer.zero_grad()
+            sp_pred = wessup(img, sp_maps.squeeze())
+            loss = criterion(sp_pred, sp_labels.squeeze())
+            loss.backward()
+            optimizer.step()
+
+        print('Loss: {}\n'.format(loss.item()))
