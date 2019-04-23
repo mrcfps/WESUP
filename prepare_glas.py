2468f7ac5ab3ef12162583f29b248979fc668711
mRcfps <1402491442@qq.com>
2019-04-23 15:35:18 +0800

    Refactor GlaS prepare script

--- prepare_glas.py
+++ prepare_glas.py
@@ -27,6 +27,8 @@ import config
 
 warnings.filterwarnings('ignore')
 
+j = os.path.join
+
 
 def build_cli_parser():
     parser = argparse.ArgumentParser('Dataset generator for GlaS challenge.')
@@ -44,7 +46,7 @@ def split_train_val_test(orig_path, val_size=0.1):
     """Split image names into training set and validation set.
     """
 
-    grade = pd.read_csv(os.path.join(orig_path, 'Grade.csv'))
+    grade = pd.read_csv(j(orig_path, 'Grade.csv'))
     grade.drop(grade.columns[1:3], axis=1, inplace=True)
 
     testA_set = grade[grade['name'].str.startswith('testA_')]['name']
@@ -61,55 +63,39 @@ def split_train_val_test(orig_path, val_size=0.1):
     return train_set, val_set, testA_set, testB_set
 
 
-def prepare_raw_images(orig_path, dst_path, names):
+def prepare_images(orig_path, dst_path, names, split=False):
     if not os.path.exists(dst_path):
         os.mkdir(dst_path)
 
-    dst_img_dir = os.path.join(dst_path, 'images')
-    dst_mask_dir = os.path.join(dst_path, 'masks')
+    dst_img_dir = j(dst_path, 'images')
+    dst_mask_dir = j(dst_path, 'masks')
     os.mkdir(dst_img_dir)
     os.mkdir(dst_mask_dir)
 
     for name in names:
         img_name = '{}.bmp'.format(name)
         mask_name = '{}_anno.bmp'.format(name)
-        orig_img_path = os.path.join(orig_path, img_name)
-        dst_img_path = os.path.join(dst_img_dir, img_name)
-        orig_mask_path = os.path.join(orig_path, mask_name)
-        dst_mask_path = os.path.join(dst_mask_dir, img_name)
 
-        # copy original image to destination
-        copyfile(orig_img_path, dst_img_path)
+        if split:
+            img = imread(os.path.join(orig_path, img_name))
+            mask = (imread(os.path.join(orig_path, mask_name)) > 0).astype('uint8')
+            img_patches = divide_image_to_patches(img, config.PATCH_SIZE, pad=False)
+            mask_patches = divide_image_to_patches(mask, config.PATCH_SIZE, pad=False)
 
-        # save binarized mask to destination
-        imsave(dst_mask_path, (imread(orig_mask_path) > 0).astype('uint8'))
+            for idx, (img_patch, mask_patch) in enumerate(zip(img_patches, mask_patches)):
+                imsave(j(dst_img_dir, f'{name}-{idx}.bmp'), np.squeeze(img_patch))
+                imsave(j(dst_mask_dir, f'{name}-{idx}.bmp'), np.squeeze(mask_patch))
+        else:
+            orig_img_path = j(orig_path, img_name)
+            dst_img_path = j(dst_img_dir, img_name)
+            orig_mask_path = j(orig_path, mask_name)
+            dst_mask_path = j(dst_mask_dir, img_name)
 
+            # copy original image to destination
+            copyfile(orig_img_path, dst_img_path)
 
-def prepare_patches(orig_path, dst_path, names):
-    if not os.path.exists(dst_path):
-        os.mkdir(dst_path)
-
-    dst_img_dir = os.path.join(dst_path, 'images')
-    dst_mask_dir = os.path.join(dst_path, 'masks')
-    os.mkdir(dst_img_dir)
-    os.mkdir(dst_mask_dir)
-
-    x, y = [], []
-
-    for name in names:
-        img_name = '{}.bmp'.format(name)
-        mask_name = '{}_anno.bmp'.format(name)
-        img = imread(os.path.join(orig_path, img_name))
-        mask = (imread(os.path.join(orig_path, mask_name)) > 0).astype('uint8')
-        x.append(divide_image_to_patches(img, config.PATCH_SIZE, pad=False))
-        y.append(divide_image_to_patches(mask, config.PATCH_SIZE, pad=False))
-
-    x = np.concatenate(x)
-    y = np.concatenate(y)
-
-    for idx, (img_patch, mask_patch) in enumerate(zip(x, y)):
-        imsave(os.path.join(dst_img_dir, f'patch-{idx}.bmp'), np.squeeze(img_patch))
-        imsave(os.path.join(dst_mask_dir, f'patch-{idx}.bmp'), np.squeeze(mask_patch))
+            # save binarized mask to destination
+            imsave(dst_mask_path, (imread(orig_mask_path) > 0).astype('uint8'))
 
 
 if __name__ == '__main__':
@@ -122,19 +108,19 @@ if __name__ == '__main__':
     if not os.path.exists(args.output):
         os.mkdir(args.output)
 
-    train_dir = os.path.join(args.output, 'train')
-    val_dir = os.path.join(args.output, 'val')
-    testA_dir = os.path.join(args.output, 'testA')
-    testB_dir = os.path.join(args.output, 'testB')
+    train_dir = j(args.output, 'train')
+    val_dir = j(args.output, 'val')
+    testA_dir = j(args.output, 'testA')
+    testB_dir = j(args.output, 'testB')
 
-    prepare_raw_images(args.dataset_path, train_dir, train_set)
+    prepare_images(args.dataset_path, train_dir, train_set)
     print('Training data is done.')
 
-    prepare_patches(args.dataset_path, val_dir, val_set)
+    prepare_images(args.dataset_path, val_dir, val_set, split=True)
     print('Validation data is done.')
 
-    prepare_raw_images(args.dataset_path, testA_dir, testA_set)
+    prepare_images(args.dataset_path, testA_dir, testA_set)
     print('TestA data is done.')
 
-    prepare_raw_images(args.dataset_path, testB_dir, testB_set)
+    prepare_images(args.dataset_path, testB_dir, testB_set)
     print('TestB data is done.')
