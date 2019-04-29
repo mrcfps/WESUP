dcc30971fd8b62459707f509cab20fa0c7108cf3
mRcfps <1402491442@qq.com>
2019-04-29 14:24:25 +0800

    Add dot annotation visualization script

---
+++ scripts/visualize_dot_annotation.py
@@ -0,0 +1,45 @@
+import argparse
+import csv
+import os
+
+import cv2
+from tqdm import tqdm
+from skimage.io import imread, imsave
+
+COLORS = (
+    (0, 255, 0),
+    (255, 0, 0),
+    (0, 0, 255),
+)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dataset_path', help='Path to dot annotation dataset')
+    parser.add_argument('-r', '--radius', type=int, default=10, help='Circle radius')
+    parser.add_argument('-o', '--output',
+                        help='Output path to store visualization results')
+    args = parser.parse_args()
+
+    output_dir = args.output or os.path.join(args.dataset_path, 'viz')
+
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    img_dir = os.path.join(args.dataset_path, 'images')
+    label_dir = os.path.join(args.dataset_path, 'labels')
+
+    print(f'Generating dot annotation visualizaion to {output_dir} ...')
+    for img_name in tqdm(os.listdir(img_dir)):
+        basename = os.path.splitext(img_name)[0]
+        img = imread(os.path.join(img_dir, img_name))
+        csvfile = open(os.path.join(label_dir, f'{basename}.csv'))
+        csvreader = csv.reader(csvfile)
+
+        for point in csvreader:
+            point = [int(d) for d in point]
+            center = (point[0])
+            viz_img = cv2.circle(img, (point[0], point[1]), args.radius, COLORS[point[2]], -1)
+
+        imsave(os.path.join(output_dir, img_name), viz_img)
+        csvfile.close()
