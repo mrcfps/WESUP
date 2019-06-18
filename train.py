Fix label propagation related metrics computation

train.py
@@ -69,7 +69,7 @@ def train_one_iteration(model, optimizer, phase, *data):
     with torch.set_grad_enabled(phase == 'train'):
         pred = model(input_)
         if phase == 'train':
-            loss = model.compute_loss(pred, target)
+            loss = model.compute_loss(pred, target, metrics=metrics)
             metrics['loss'] = loss.item()
 
             loss.backward()